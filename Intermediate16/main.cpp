88db32ef40fe8f6e9aa9d1d5cd57f5d07ea30122
BrownBoxMothballsAndBowlingShoes <Atticuscyoung@gmail.com>
2024-04-14 21:49:46 -0700

    Added functions for calculating and printing Annual Sales and Years for each sale

--- Intermediate16/main.cpp
+++ Intermediate16/main.cpp
@@ -6,9 +6,52 @@
 //
 
 #include <iostream>
+using namespace std;
+
+const double growthRate = 0.055;
+double sales;
+int years = 0;
+vector<double> salesList;
+vector<int> yearsList;
+
+void printSales(vector <double> const &a) {
+   cout << "Sales: ";
+
+   for(int i=0; i < a.size(); i++)
+   cout << a.at(i) << ' ';
+}
+
+void printYears(vector <int> const &a) {
+   cout << "Years: ";
+
+   for(int i=0; i < a.size(); i++)
+   cout << a.at(i) << ' ';
+}
+
+static void annualSalesIncrease(double sales) {
+    while (1) {
+        cout << "Enter sales price (enter -1 as the terminator) \n";
+        cin >> sales;
+        if (sales == -1) {
+            goto output;
+        }
+        
+        while (sales < 150000) {
+            sales += sales * growthRate;
+            years++;
+        }
+        salesList.push_back(sales);
+        yearsList.push_back(years);
+        years = 0;
+    }
+    
+    output:
+    printSales(salesList);
+    cout << "\n";
+    printYears(yearsList);
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    annualSalesIncrease(sales);
 }
+
