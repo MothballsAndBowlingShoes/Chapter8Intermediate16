2ee801b1144416a049cec9d8be9239cf8e965e92
BrownBoxMothballsAndBowlingShoes <Atticuscyoung@gmail.com>
2024-04-14 20:46:53 -0700

    Initial Commit

---
+++ Intermediate16/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Intermediate16
+//
+//  Created by Atticus Young on 4/14/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
